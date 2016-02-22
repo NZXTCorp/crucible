5785a852ff08540504a3249e608b8753c71da92a
Ruwen Hahn <ruwen@forge.gg>
2016-02-22 19:51:54 +0100

    Remove unnecessary function declaration

--- AnvilRendering/TaksiInput/InputHooks.h
+++ AnvilRendering/TaksiInput/InputHooks.h
@@ -12,7 +12,4 @@ bool HookDI( UINT_PTR dll_base );
 void UnhookDI( void );
 #endif
 
-// process window messages relating to input. return true if we should eat the message (return 0 from HookedWndProc instead of calling the game's original handler)
-bool InputWndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
-
 #endif
