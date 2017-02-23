124981433e178f6c37348e713e6afb8bd57120c2
Ruwen Hahn <ruwen@forge.gg>
2017-02-23 14:46:34 -0800

    Fix copy typo

--- AnvilRendering/GAPI_render/NewIndicator.cpp
+++ AnvilRendering/GAPI_render/NewIndicator.cpp
@@ -152,7 +152,7 @@ wstring hotkeyHelpText_Basic[HOTKEY_QTY] = {
 	L"Open the streaming overlay",
 	L"Start or stop streaming",
 	L"Mute/unmute microphone",
-	L"Save an replay clip",
+	L"Save a replay clip",
 	L"Save an instant clip",
 };
 
